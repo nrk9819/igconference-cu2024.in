9cb4186052a0a945b3f37457a4fb7d1d5179d3d1
nrk9819 <118840851+nrk9819@users.noreply.github.com>
2023-10-18 01:43:23 +0530

    add sitemap plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  site: 'https://iigconference-cu2024.com',
+  site: "https://iigconference-cu2024.com",
   integrations: [
     tailwind({
       //   applyBaseStyles: false,
     }),
+    sitemap({}),
   ],
 });
