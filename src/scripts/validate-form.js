7cdf50d9d4d622f825130ca31a5f3e4c769fc1d0
nrk9819 <118840851+nrk9819@users.noreply.github.com>
2023-10-19 09:30:28 +0530

    add form validator

---
+++ src/scripts/validate-form.js
@@ -0,0 +1,39 @@
+export default function validateForm(form) {
+  const inputs = form.querySelectorAll("input[required]");
+  const errors = [];
+
+  for (const input of inputs) {
+    const fieldType = input.getAttribute("type");
+    const value = input.value.trim();
+
+    // check for email field
+    if (fieldType === "email" && !validateEmail(value)) {
+      errors.push(`Invalid email in field '${input.name}'.`);
+    }
+
+    // check for phone field
+    if (fieldType === "tel" && !validatePhoneNumber(value)) {
+      errors.push(`Invalid phone number in field '${input.name}'.`);
+    }
+
+    if (value.length === 0) {
+      errors.push(`Field '${input.name}' is required.`);
+    }
+  }
+
+  if (errors.length > 0) {
+    return `${errors.join("\n")}`;
+  }
+
+  return true;
+}
+
+function validateEmail(email) {
+  const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+  return emailPattern.test(email);
+}
+
+function validatePhoneNumber(phone) {
+  const phonePattern = /^\d{10,}$/;
+  return phonePattern.test(phone);
+}
